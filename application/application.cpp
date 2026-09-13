ed34e921a12661c6647a7c122f9eda78f4a5cc65
Infinitusvoid <84730862+Infinitusvoid@users.noreply.github.com>
2026-09-14 01:51:48 +0200

    update

--- application/application.cpp
+++ application/application.cpp
@@ -396,7 +396,129 @@ namespace Sketch
 
 		Raster::draw_triangle_wireframe(*image, a.x, a.y, b.x, b.y, c.x, c.y, color);
 
-		ImageRGBA_::save_png(*image, "run_sketch_0001_visualization_edge_function.png");
+		ImageRGBA_::save_png(*image, "run_sketch_0001_visualization_edge_function_edge_AB.png");
+
+		ImageRGBA_::free_image(image);
+
+		return 0;
+	}
+
+	int run_sketch_0002_visualization_edge_function_edge_BC()
+	{
+		std::cout << "\n";
+		std::cout << "----------------------- \n";
+
+		std::cout << "run_sketch_0001_visualization_edge_function\n";
+		std::cout << "----------------------- \n";
+		// we define the triangle
+
+		struct Point
+		{
+			int x;
+			int y;
+		};
+
+		const int image_size = 1024;
+
+		Point a((image_size / 10) * 1, (image_size / 10) * 9);
+		Point b((image_size / 10) * 4, (image_size / 10) * 2);
+		Point c((image_size / 10) * 9, (image_size / 10) * 9);
+
+		ImageRGBA* image = ImageRGBA_::create(image_size, image_size);
+
+		RGBA color;
+		color.r = 0;
+		color.g = 200;
+		color.b = 200;
+		color.a = 255;
+
+		// BC
+		{
+			int width = ImageRGBA_::get_width(*image);
+			int height = ImageRGBA_::get_height(*image);
+
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					float value = Math::edge_function(float(b.x), float(b.y), float(c.x), float(c.y), float(x), float(y));
+
+					if (value > 0)
+					{
+						ImageRGBA_::set_pixel(*image, x, y, RGBA(0, 100, 100, 255));
+					}
+					else
+					{
+						ImageRGBA_::set_pixel(*image, x, y, RGBA(0, 0, 0, 255));
+					}
+				}
+			}
+		}
+
+		Raster::draw_triangle_wireframe(*image, a.x, a.y, b.x, b.y, c.x, c.y, color);
+
+		ImageRGBA_::save_png(*image, "run_sketch_0002_visualization_edge_function_edge_BC.png");
+
+		ImageRGBA_::free_image(image);
+
+		return 0;
+	}
+
+	int run_sketch_0003_visualization_edge_function_edge_CA()
+	{
+		std::cout << "\n";
+		std::cout << "----------------------- \n";
+
+		std::cout << "run_sketch_0001_visualization_edge_function\n";
+		std::cout << "----------------------- \n";
+		// we define the triangle
+
+		struct Point
+		{
+			int x;
+			int y;
+		};
+
+		const int image_size = 1024;
+
+		Point a((image_size / 10) * 1, (image_size / 10) * 9);
+		Point b((image_size / 10) * 4, (image_size / 10) * 2);
+		Point c((image_size / 10) * 9, (image_size / 10) * 9);
+
+		ImageRGBA* image = ImageRGBA_::create(image_size, image_size);
+
+		RGBA color;
+		color.r = 0;
+		color.g = 200;
+		color.b = 200;
+		color.a = 255;
+
+		// BC
+		{
+			int width = ImageRGBA_::get_width(*image);
+			int height = ImageRGBA_::get_height(*image);
+
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					float value = Math::edge_function(float(c.x), float(c.y), float(a.x), float(a.y), float(x), float(y));
+
+					if (value > 0)
+					{
+						ImageRGBA_::set_pixel(*image, x, y, RGBA(0, 100, 100, 255));
+					}
+					else
+					{
+						ImageRGBA_::set_pixel(*image, x, y, RGBA(0, 0, 0, 255));
+					}
+				}
+			}
+		}
+
+		Raster::draw_triangle_wireframe(*image, a.x, a.y, b.x, b.y, c.x, c.y, color);
+
+		ImageRGBA_::save_png(*image, "run_sketch_0003_visualization_edge_function_edge_CA.png");
 
 		ImageRGBA_::free_image(image);
 
@@ -415,7 +537,11 @@ namespace Sketch
 int run_application()
 {
 	// return Sketch::run_sketch_0000();
-	return Sketch::run_sketch_0001_visualization_edge_function();
+	Sketch::run_sketch_0001_visualization_edge_function_edge_AB();
+	Sketch::run_sketch_0002_visualization_edge_function_edge_BC();
+	Sketch::run_sketch_0003_visualization_edge_function_edge_CA();
+
+	return 0;
 }
 
 
